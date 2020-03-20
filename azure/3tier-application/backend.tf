01d3fe4e0b28216a0d1c5d715930013ad5a7233b
Ali SaIT Karamanli <55097581+alisahp@users.noreply.github.com>
2020-03-19 21:32:40 -0500

    Update backend.tf

--- azure/3tier-application/backend.tf
+++ azure/3tier-application/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "StorageAccount-ResourceGroup"
     storage_account_name = "alisait1"
     container_name       = "alisaitcontainer"
-    access_key           = wS2CX7BRX0gwIpprttVZfoeI+NUhrc6NxGAjDdTQ8fihI9urVB16o65D5GPZ8tqMM31mDME0bd8FDMSnklOGIA==
+    access_key           = "......"
     key                  = "prod.terraform.tfstate"
   }
 }
